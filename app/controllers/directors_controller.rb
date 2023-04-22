6317a44d8917496947166c745a0a22f9984540f2
Ryan Gilboy <rgilboy@chicagobooth.edu>
2023-04-22 22:54:12 +0000

    Directors details almost done

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,46 @@
+class DirectorsController < ApplicationController
+
+def index
+
+  @list_of_directors = Director.all
+
+
+  render({:template => "director_templates/index.html.erb"})
+
+end
+
+def wisest
+
+  @list_of_directors = Director.all.where.not(:dob => 0)
+
+  @old_to_young = @list_of_directors.order({:dob => :asc})
+
+  @oldest = @old_to_young.first
+
+
+
+  render({:template => "director_templates/eldest.html.erb"})
+end
+
+def youngest
+
+  @list_of_directors = Director.order({:dob => :desc})
+
+  @youngest = @list_of_directors.first
+
+  render({:template => "director_templates/youngest.html.erb"})
+
+end
+
+def details
+  @director_id = params.fetch("id").to_i
+
+  @director = Director.where(:id => @director_id).first
+
+  render({:template => "director_templates/details.html.erb"})
+
+end
+
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", {:controller => "directors", :action => "index"})
+
+  get("/directors/eldest", {:controller => "directors", :action => "wisest"})
+
+  get("/directors/youngest", {:controller => "directors", :action => "youngest"})
+
+  get("/directors/:id", {:controller => "directors", :action => "details"})
+
+
 end
